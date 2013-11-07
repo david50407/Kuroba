92bdaf6fd583d8cc0a260b1c68eb1d020260ac44
David Kuo <s50407s@gmail.com>
2013-11-07 21:51:17 +0800

    Message list/view mockup.

--- app/views/layouts/_navMenu.html.php
+++ app/views/layouts/_navMenu.html.php
@@ -1,7 +1,12 @@
 <div class="pure-menu pure-menu-open">
 	<ul>
-		<li class="pure-menu-selected"><a href="#">Guestbook</a></li>
-		<li><a href="#">Post</a></li>
+		<li class="pure-menu-selected"><a href="#">General</a></li>
+		<li><a href="#">Programming</a></li>
+		<li><a href="#">Comic & Douga</a></li>
+		<li><a href="#">Music & Movie</a></li>
+		<li><a href="#">Testing</a></li>
+		<li class="pure-menu-heading">Admin</li>
 		<li><a href="#">Setting</a></li>
+		<li><a href="#">Report BUG</a></li>
 	</ul>
 </div>
